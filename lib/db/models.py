36a00d40ca3775581c7f93583a885b528bdb321b
William Wong <william.wai.lim.wong@gmail.com>
2023-08-22 13:51:31 -0400

    adds __repr__ to classes

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import create_engine, func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///library.db')
@@ -15,7 +15,18 @@ class Patron(Base):
     last_name = Column(String())
     phone = Column(Integer())
     card_num = Column(Integer())
-    home_branch = (String())
+    home_branch_id = (Integer(), ForeignKey('libraries.id'))
+
+    requests = relationship("Request", backref='patron')
+
+    def __repr__(self):
+
+        return f'Patron(id={self.id}, ' + \
+            f'first_name={self.first_name}, ' + \
+            f'last_name={self.last_name}, ' + \
+            f'card_num={self.card_num})'
+    
+
 
 
 class Book(Base):
@@ -27,7 +38,16 @@ class Book(Base):
     author_first_name = Column(String())
     author_last_name = Column(String())
     ISBN = Column(Integer())
-    branch = (String())
+    branch_id = (Integer(), ForeignKey('libraries.id'))
+
+    requests = relationship('Request', backref='book')
+
+    def __repr__(self):
+
+        return f'Book(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'author_last_name={self.author_last_name}, ' + \
+            f'ISBN={self.ISBN})'
 
 class Library(Base):
     __tablename__ = 'libraries'
@@ -35,3 +55,29 @@ class Library(Base):
     id = Column(Integer(), primary_key=True)
     branch = (String())
     address = (String())
+
+    def __repr__(self):
+
+        return f'Library(id={self.id}, ' + \
+            f'branch={self.branch}, ' + \
+            f'address={self.address})'
+
+class Request(Base):
+    __tablename__ = 'requests'
+
+    id = Column(Integer(), primary_key=True)
+    patron_id = Column(Integer(), ForeignKey('patrons.id'))
+    book_id = Column(Integer(), ForeignKey('books.id'))
+    branch_id = Column(Integer(), ForeignKey('libraries.id'))
+    queue = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+
+    def __repr__(self):
+
+        return f'Request(id={self.id}, ' + \
+            f'patron_id={self.patron_id}, ' + \
+            f'book_id={self.book_id}, ' + \
+            f'branch_id={self.branch_id}, ' + \
+            f'queue_id={self.queue})'
+
+
